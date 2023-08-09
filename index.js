9c6ea4c4c80150823f0b25afeffc2f6d869c09b5
Vineeth Serigar <vineeth.serigar@netcracker.com>
2023-08-09 15:11:08 +0530

    [master] Initial setup, socket install & client <-> server communication

---
+++ index.js
@@ -0,0 +1,16 @@
+import { Server } from "socket.io";
+
+const io = new Server(8000, { cors: true });
+
+const emailToSocketId = new Map();
+const socketIdToEmail = new Map();
+
+io.on("connection", (socket) => {
+  console.log("Socket Connected. 🚀", socket.id);
+
+  socket.on("meeting:join", (data) => {
+    emailToSocketId.set(data.email, socket.id);
+    socketIdToEmail.set(socket.id, data.email);
+    io.to(socket.id).emit("meeting:join", data);
+  });
+});
